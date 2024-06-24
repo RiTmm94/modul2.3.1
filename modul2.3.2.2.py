25908a5111f9f55a947e08427fbe08457d76b0c2
RiTmm94 <shabalin.kostya2015@yandex.ru>
2024-06-24 12:46:32 +0500

    Add files via upload

---
+++ modul2.3.2.2.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while 'my_list > 0':
+    for a in my_list:
+        b = 0
+        if a > b:
+            print(a)
+            continue
+        else:
+            break
